f765af7fa73a77a79cc11bd7f673f8e9677e916f
sige <sige@configcat.com>
2019-08-20 14:56:33 +0200

    update sample app [skip ci]

--- samples/console/main.go
+++ samples/console/main.go
@@ -8,10 +8,10 @@ import (
 func main() {
 	client := configcat.NewClient("PKDVCLf-Hq-h-kCzMp-L7Q/psuH7BGHoUmdONrzzUOY7A")
 
-	// create a user object to identify the caller
-	user := configcat.NewUser("key")
+	// create a user object to identify your user (optional)
+	user := configcat.NewUser("##SOME-USER-IDENTIFICATION##")
 
-	// get individual config values identified by a key for a user
+	// get individual config values identified by a key and a user object
 	value := client.GetValueForUser("keySampleText", "", user)
 
 	fmt.Println("keySampleText: ", value)
