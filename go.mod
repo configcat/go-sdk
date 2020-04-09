7f10269c9b71db94c55bf7c49e22ab95529942a2
sige <sige@configcat.com>
2020-04-09 23:04:13 +0200

    updating sample

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/blang/semver v3.5.1+incompatible
+	github.com/configcat/go-sdk/v5 v5.0.2
 	github.com/sirupsen/logrus v1.4.2
 )
--- samples/console/main.go
+++ samples/console/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/configcat/go-sdk/v4"
+	"github.com/configcat/go-sdk/v5"
 )
 
 func main() {
 	client := configcat.NewCustomClient("PKDVCLf-Hq-h-kCzMp-L7Q/HhOWfwVtZ0mb30i9wi17GQ",
-		configcat.ClientConfig{Logger: configcat.DefaultLogger(configcat.LogLevelInfo),
-	})
+		configcat.ClientConfig{Logger: configcat.DefaultLogger(configcat.LogLevelInfo)})
 
 	// create a user object to identify your user (optional)
 	user := configcat.NewUserWithAdditionalAttributes("##SOME-USER-IDENTIFICATION##", "configcat@example.com", "", nil)
