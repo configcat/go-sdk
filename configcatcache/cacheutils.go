0a5d694914d3478fa2ed0eb0711800be59926827
Peter Csajtai <peter.csajtai@outlook.com>
2023-07-25 15:48:21 +0200

    Consistent error messages

--- configcatcache/cacheutils.go
+++ configcatcache/cacheutils.go
@@ -15,20 +15,16 @@ const newLineByte byte = '\n'
 // CacheSegmentsFromBytes deserializes a cache entry from a specific format used by the SDK.
 func CacheSegmentsFromBytes(cacheBytes []byte) (fetchTime time.Time, eTag string, config []byte, err error) {
 	fetchTimeIndex := bytes.IndexByte(cacheBytes, newLineByte)
-	if fetchTimeIndex == -1 {
-		return time.Time{}, "", nil, fmt.Errorf("fetch time segment of the cache entry not found")
+	eTagIndex := bytes.IndexByte(cacheBytes[fetchTimeIndex+1:], newLineByte)
+	if fetchTimeIndex == -1 || eTagIndex == -1 {
+		return time.Time{}, "", nil, fmt.Errorf("number of values is fewer than expected")
 	}
 
 	fetchTimeBytes := cacheBytes[:fetchTimeIndex]
 
 	fetchTimeMs, err := strconv.ParseInt(string(fetchTimeBytes), 10, 64)
 	if err != nil {
-		return time.Time{}, "", nil, err
-	}
-
-	eTagIndex := bytes.IndexByte(cacheBytes[fetchTimeIndex+1:], newLineByte)
-	if eTagIndex == -1 {
-		return time.Time{}, "", nil, fmt.Errorf("etag segment of the cache entry not found")
+		return time.Time{}, "", nil, fmt.Errorf("invalid fetch time: %s. %v", fetchTimeBytes, err)
 	}
 
 	eTagBytes := cacheBytes[fetchTimeIndex+1 : fetchTimeIndex+eTagIndex+1]
